1bbfa58f4ade7f4c45e6abb6ccb3847d2ab05b9d
Raffael Thobias <70666000+dashp21@users.noreply.github.com>
2023-03-15 23:40:22 -0300

    Update CrawlerFlask.py

--- CrawlerFlask.py
+++ CrawlerFlask.py
@@ -1,53 +1,11 @@
-import io
-import json
-import os
-import requests
-
-from PyPDF2 import PdfReader
-from flask import Flask, jsonify
-
-from nltk.sentiment import SentimentIntensityAnalyzer
-
+from flask import Flask
 
 app = Flask(__name__)
-sia = SentimentIntensityAnalyzer()
-keywords = ["pé diabético", "neuropatia diabética", "úlcera diabética", "amputação", "neuropatia periférica", "circulação sanguínea", "tratamento", "diagnóstico", "prevenção", "complicações", "diabetes mellitus", "cuidados", "terapia", "monitoramento"]
 
+@app.route('/')
+def hello_world():
+	return 'Hello World Again'
 
-@app.route('/results')
-def results():
-    output_file = "resultados.json"
-    results = []
-
-    for url in ["https://iwgdfguidelines.org/wp-content/uploads/2020/12/Brazilian-Portuguese-translation-IWGDF-Guidelines-2019.pdf",
-                "https://portal.unicap.br/documents/475032/672293/ebook+Livro+pe+diabetico-2020.pdf/36b829a4-e588-cee9-e4ea-89cf8cf50fc6?t=1608742383653#:~:text=Segundo%20o%20Minist%C3%A9rio%20da%20Sa%C3%BAde,diabetes%20(BRASIL%2C%202016)"]:
-        response = requests.get(url)
-
-        with io.BytesIO(response.content) as data:
-            reader = PdfReader(data)
-            for i, page in enumerate(reader.pages):
-                text = page.extract_text()
-                lines = text.split('\n')
-                for j, line in enumerate(lines):
-                    if _contains_keywords(line):
-                        result = {
-                            'page_number': i + 1,
-                            'position': j,
-                            'context': lines[j-2:j+2],
-                            'text': line.strip(),
-                            'sentiment_score': sia.polarity_scores(line)['compound']
-                        }
-                        results.append(result)
-
-    with open(output_file, "w", encoding="utf-8") as f:
-        json.dump(results, f, indent=4, ensure_ascii=False)
-
-    return jsonify(results)
-
-
-def _contains_keywords(text):
-    return any(word in text.lower() for word in keywords)
-
-
+# main driver function
 if __name__ == '__main__':
-    app.run()
+	app.run()
